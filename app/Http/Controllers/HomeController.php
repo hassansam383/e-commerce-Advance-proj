f0fc6c46527736e6c1df87db26835aa2c0270f92
hassansam383 <hassansam383@gmail.com>
2023-08-24 12:13:30 +0500

    showed the products from database to userpage

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,13 +5,15 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+use App\Models\Product;
 
 class HomeController extends Controller
 {
 
     public function index()
         {
-            return view('home.userpage');
+            $product=product::paginate(10);
+            return view('home.userpage', compact('product'));
         }
     
 
@@ -22,7 +24,8 @@ class HomeController extends Controller
                 if ($usertype == '1') {
                     return view('admin.home');
                 } else {
-                    return view('home.userpage');
+                    $product=product::paginate(10);
+                    return view('home.userpage', compact('product'));
                 }
             }
         }
