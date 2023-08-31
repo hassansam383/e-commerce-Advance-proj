7840a62538ea91b96491ddad3981ffb6192aaa8a
hassansam383 <hassansam383@gmail.com>
2023-08-31 10:44:04 +0500

    showed the products drtails

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -29,6 +29,17 @@ class HomeController extends Controller
                 }
             }
         }
+
+
+        public function product_details($id)
+
+        {
+
+            $product=product::find($id);
+
+            return view('home.product_details', compact('product'));
+
+        }
         
         
         
